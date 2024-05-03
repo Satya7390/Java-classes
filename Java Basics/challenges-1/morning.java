13e0f4909881fe76721a356444dc9181f1f4f2f9
satya <satyanandgupta845@gmail.com>
2024-05-03 07:50:18 +0530

    basics of java

---
+++ Java Basics/challenges-1/morning.java
@@ -0,0 +1,15 @@
+
+// package challenges;
+import java.lang.*;  
+
+public class morning {
+    public static void main(String[] args) {
+        System.out.println("Good Morning");
+    }
+    
+}
+
+
+// D:\Media\Online Courses\Java Tutorials\JAVA programming\challenges> javac morning.java
+// PS D:\Media\Online Courses\Java Tutorials\JAVA programming\challenges> java morning
+// Good Morning
---
+++ Java Basics/challenges-1/satya.java
@@ -0,0 +1,6 @@
+public class satya {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Satya !!");
+    }
+}
