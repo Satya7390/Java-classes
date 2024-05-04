a72e7ff2e70ba268c4f9c955db0915f7932f3f22
satya <satyanandgupta845@gmail.com>
2024-05-04 07:43:32 +0530

    pattern done

---
+++ Java Basics/challenges-1/patterns/ReverseRightHalfPyramid.java
@@ -0,0 +1,9 @@
+public class ReverseRightHalfPyramid {
+    public static void main(String[] args) {
+        System.out.println("* * * * * ");
+                System.out.println("* * * * ");
+                System.out.println("* * *");
+                System.out.println("* *");
+                System.out.println("*"); 
+    }
+}
---
+++ Java Basics/challenges-1/patterns/RightHalfPyramid.java
@@ -0,0 +1,16 @@
+public class RightHalfPyramid{
+    public static void main (String [] args){
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * * ");
+        System.out.println("* * * * * ");
+    }
+}
+
+
+
+
+
+       
+
