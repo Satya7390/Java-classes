6d85ddffeea27ec07eec748b539f87b4702bcbad
satya <satyanandgupta845@gmail.com>
2024-05-08 07:44:22 +0530

    Type conversion done

---
+++ Lectures/TypeConversion/TypeConersion.java
@@ -0,0 +1,21 @@
+// public package TypeConversion;
+
+// implicit
+
+class TypeConersion {
+
+    public static void main(String[] args) {
+        float myFloat = 5L;
+        System.out.println(myFloat);
+    }
+}
+
+// Explicit
+
+// class TypeConersion {
+
+//     public static void main(String[] args) {
+//         int myInt = (int)5.34;
+//         System.out.println(myInt);
+//     }
+// }
