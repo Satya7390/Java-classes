53e14b0b65975eabddaef1582b5f252787550eb2
satya <satyanandgupta845@gmail.com>
2024-05-08 07:41:54 +0530

    Challenge done

--- Challenges/3_challenges/Add.java
+++ Challenges/3_challenges/Add.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Add {
     public static void main(String[] args) {
         Scanner add = new Scanner(System.in);
-        System.out.println("Are you excited for addition ? ");
+        System.out.print("Are you excited for addition ? ");
         String ans = add.nextLine();
-        System.out.println("please enter your first number");
+        System.out.print("please enter your first number: ");
         int num1 = add.nextInt();
-        System.out.println("please enter your second number");
+        System.out.print("please enter your second number: ");
         int num2 = add.nextInt();
         int Fans=num1+num2;
-        System.out.println("your answer is : "+Fans);
+        System.out.print("your answer is : "+Fans);
     }
 }
--- Challenges/3_challenges/userInput.java
+++ Challenges/3_challenges/userInput.java
@@ -5,9 +5,8 @@ import java.util.Scanner;
 public class userInput {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Please enter your name:");
+        System.out.print("Please enter your name: ");
         String name =input.nextLine();
         System.out.println("Welcome "+name+" to KG Coding");
     }
-    
-}
+    }
