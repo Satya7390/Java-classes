0559ec34337231bf41daeb7f931c6685f8762dc0
satya <satyanandgupta845@gmail.com>
2024-05-08 07:46:27 +0530

    True False Done

---
+++ Lectures/DataTypes/Lecture-1/variables.java
@@ -0,0 +1,21 @@
+public class variables {
+    public static void main(String[] args) {
+        int myNumber;
+        myNumber = 10;
+        System.out.println(myNumber);
+        myNumber++;
+        System.out.println(myNumber);
+        myNumber = -33;
+        System.out.println(myNumber);
+        float myFloat=5.0f;
+        System.out.println(myFloat);
+        float myFloat2 = 5;
+        System.out.println(myFloat2);
+        double myDub=4.4444;
+        System.out.println(myDub);
+        boolean isMynameSatya = true;
+        System.out.println(isMynameSatya);
+        String wishes ="Hii Satya";
+        System.out.println(wishes);
+    }
+}
