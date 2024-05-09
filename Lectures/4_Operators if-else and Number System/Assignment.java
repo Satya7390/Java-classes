2e46dcf89c4deab4dc4552ef6b3e89ad4b0d7ab4
satya <satyanandgupta845@gmail.com>
2024-05-09 07:56:03 +0530

    Assignment operatotr

---
+++ Lectures/4_Operators if-else and Number System/Assignment.java
@@ -0,0 +1,13 @@
+// package Lectures.4_Operators if-else and Number System;
+
+public class Assignment {
+    public static void main(String[] args) {
+        int myInt = 5;
+        System.out.println(myInt);
+        int newInt = myInt;
+        System.out.println(newInt);
+
+       // 9= int NInt; wrong way to write this
+    }
+    
+}
