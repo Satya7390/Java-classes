07d7b60a6c5f5f30ca63440edbaa80e836f53a5c
satya <satyanandgupta845@gmail.com>
2024-05-10 07:19:26 +0530

    Arithmetic operator done

---
+++ Lectures/4_Operators if-else and Number System/Arithmetic.java
@@ -0,0 +1,19 @@
+
+
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        int a=20;
+        int b=6;
+        System.out.println(a+b); // 26
+        System.out.println(a-b); // 14
+        System.out.println(a*b); // 120
+        System.out.println(a/b); // 3
+        System.out.println(a%b); // 2
+
+        double c=9;
+        double d=4;
+        System.out.println(c/d); // 2.25
+        System.out.println(c%d); // 1.0
+    }
+}
