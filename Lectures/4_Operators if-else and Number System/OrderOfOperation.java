8432918a3d2f2276b285ac4a4cac8154b631eabd
satya <satyanandgupta845@gmail.com>
2024-05-10 07:41:25 +0530

    OrderOfOperation done

---
+++ Lectures/4_Operators if-else and Number System/OrderOfOperation.java
@@ -0,0 +1,7 @@
+public class OrderOfOperation {
+    public static void main(String[] args) {
+        System.out.println(9-7+8); // 10
+        System.out.println(5-6*4/3+5); // 2
+        System.out.println(5+6*(7/4/3)); // 5
+    }
+}
