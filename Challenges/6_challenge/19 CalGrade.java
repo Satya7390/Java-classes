103003a9e3e3759966fef1158d18d95f961d5da9
satya <satyanandgupta845@gmail.com>
2024-05-17 07:16:18 +0500

    Challenge 19 & 20 Done

---
+++ Challenges/6_challenge/19 CalGrade.java
@@ -0,0 +1,45 @@
+ 
+//  19.Create a program that calculates grades based on marks
+
+//  A -> above 90%
+ 
+//  B -> above 75%
+ 
+//  C -> above 60%
+ 
+//  D -> above 30%
+ 
+//  F -> below 30%
+
+import java.util.Scanner;
+
+class  CalGrade {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nWelcome to Grade Calculator!!\n");
+        System.out.print("Please Enter your Percentage : ");
+
+        Float per = input.nextFloat();
+
+        if (per > 100){
+            System.out.println("Warning!! , Your Enter Percentage must be 100 or Less than 100 .");
+        }
+        else if ( per <= 100 && per >= 90){
+            System.out.println("Congratulations , Your Grade is A");
+        }
+        else if( per < 90 && per >= 75){
+            System.out.println("Well done , Your Grade is B");
+        }
+        else if( per < 75 && per >= 60){
+            System.out.println("Good Job!! Your Grade is C");
+        }
+        else if( per < 60 && per >= 30){
+            System.out.println("Need to Improve!, Your Grade is D");
+        }
+        else {
+            System.out.println("Sorry!! , You have not Clear your Examination This Time Try Again");
+        }
+    }
+    
+}
---
+++ Challenges/6_challenge/20 AgeGroup.java
@@ -0,0 +1,41 @@
+
+// 20.Create a program that categorize a person into different age groups
+
+// Child -> below 13
+
+// Teen -> below 20
+
+// Adult -> below 60
+
+// Senior-> above 60
+
+import java.util.Scanner;
+
+class  AgeGroup {
+
+    public static void main(String[] args) {
+
+        Scanner input= new Scanner(System.in);
+        System.out.println("\nWelcome to Age Group Calculator!!\n");
+
+        System.out.print("Please Enter your AGE : ");
+        int age = input.nextInt();
+
+        if (age >60 ){
+            System.out.println("Your are a Senior Person!!");
+        }
+        
+        else if (age < 60 && age > 20) {
+            System.out.println("You are a Adult Person");
+        }
+
+        else if (age < 20 && age > 13) {
+            System.out.println("You are a Teen Person");
+        }
+        else {
+            System.out.println("You are a Child!! ");
+        }
+
+        
+    }
+}
