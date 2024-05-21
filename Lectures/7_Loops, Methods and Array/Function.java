9330c81f8e3f2322bd65118521eb81ea26495bed
satya <satyanandgupta845@gmail.com>
2024-05-21 08:26:01 +0530

    Basic function concepts completed

---
+++ Lectures/7_Loops, Methods and Array/Function.java
@@ -0,0 +1,22 @@
+public class Function {
+    public static void main(String[] args) {
+        System.out.println("\nIn Main method");
+        greetUser();
+        System.out.println("Method calling Completed");
+        greetUser();
+        
+    }
+    public static void greetUser() {
+        System.out.println("Good Morning Satya!!");
+        
+    }
+    
+}
+
+
+// output
+
+// In Main method
+// Good Morning Satya!!
+// Method calling Completed
+// Good Morning Satya!!
