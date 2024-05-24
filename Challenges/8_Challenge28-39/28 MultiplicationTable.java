bb7ef206ed8ecc83a201a18fc1290da8c62454b6
satya <satyanandgupta845@gmail.com>
2024-05-24 22:10:44 +0530

    Challange 28 multiplication table is done

--- Challenges/8_Challenge28-39/28 MultiplicationTable.java
+++ Challenges/8_Challenge28-39/28 MultiplicationTable.java
@@ -45,3 +45,22 @@ import java.util.Scanner;
     }
 
 }
+
+
+// output :
+
+// Hey !! , Welcome to Multiplication Table
+
+// Now , please tell me what number do you want to print table : 5
+
+// Your Table of 5 is ready now !!
+// 5*1 = 5
+// 5*2 = 10
+// 5*3 = 15
+// 5*4 = 20
+// 5*5 = 25
+// 5*6 = 30
+// 5*7 = 35
+// 5*8 = 40
+// 5*9 = 45
+// 5*10 = 50
