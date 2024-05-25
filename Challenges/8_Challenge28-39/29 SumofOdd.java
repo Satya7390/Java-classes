5dd8bb2f80524a27132c80cae96781cd3eaa599b
satya <satyanandgupta845@gmail.com>
2024-05-25 23:01:43 +0530

    File created for summ of odd numbers

---
+++ Challenges/8_Challenge28-39/29 SumofOdd.java
@@ -0,0 +1,19 @@
+
+
+// 29. Create a program to sum all odd numbers from 1 to a specified number N.
+
+import java.util.Scanner;
+
+/**
+ * 29 SumofOdd
+ */
+ class  SumofOdd {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hii Satya!!");
+    }
+    
+}
+
