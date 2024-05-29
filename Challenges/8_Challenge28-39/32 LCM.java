b21bbeba53fcc1cfbef1b3c7c6f1bc61fc591e9c
satya <satyanandgupta845@gmail.com>
2024-05-29 23:12:32 +0530

    Challenge 32(LCM) done

--- Challenges/8_Challenge28-39/32 LCM.java
+++ Challenges/8_Challenge28-39/32 LCM.java
@@ -1,8 +1,34 @@
 
 // 32. Create a program to find the Least Common Multiple (LCM) of two numbers.
 
- class  LCM {
+import java.util.Scanner;
+
+class LCM {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Calculate LCM of Two Numbers!!");
+
+        System.out.print("Please Enter your first Number : ");
+        int first = input.nextInt();
+        System.out.print("Now,Please enter your second number : ");
+        int second = input.nextInt();
+
+        int lcm = lcm(first, second);
+        System.out.println("Your LCM of Two Number is : " + lcm);
     }
+
+    public static int lcm(int first, int second) {
+
+        int i = 1;
+        while (true) {
+            int fact = first * i;
+            if (fact % second == 0) {
+                return fact;
+
+            }
+            i++;
+        } // return 0; not reachable that,s whay I,m not using return here
+
+    }
+
 }
