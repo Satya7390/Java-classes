9b88c1d13068ce15082cb4c2f55e3c25124a5822
satya <satyanandgupta845@gmail.com>
2024-05-30 22:41:52 +0530

    Challenge 33 done GCD

--- Challenges/8_Challenge28-39/33 GCD.java
+++ Challenges/8_Challenge28-39/33 GCD.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-class  GCD {
+class GCD {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Wecome to Calculate Greatest Common Divisor of two Numbers!!");
@@ -17,28 +17,34 @@ class  GCD {
 
     }
 
-    public static int gcd(int num1 , int num2){
+    public static int gcd(int num1, int num2) {
         int leastNum = leastNum(num1, num2);
         int gcd = 1;
         int i = 2;
         while (i <= leastNum) {
             if (num1 % i == 0 && num2 % i == 0) {
                 gcd = i;
-                
+
             }
             i++;
-            
+
         }
         return gcd;
     }
 
-    public static int leastNum(int num1,int num2){
-        if (num1 < num2 ){
+    public static int leastNum(int num1, int num2) {
+        if (num1 < num2) {
             return num1;
-        }
-        else{
+        } else {
             return num2;
         }
     }
-    
+
 }
+
+
+// Output :
+// Wecome to Calculate Greatest Common Divisor of two Numbers!!
+// please enter first number : 45
+// please enter second number : 10
+// Your GCD of Two Numbers is : 5
