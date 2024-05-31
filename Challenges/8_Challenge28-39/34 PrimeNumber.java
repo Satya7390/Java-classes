b08a7481c31d8f54d5c25d1263da9161a7f428d5
satya <satyanandgupta845@gmail.com>
2024-05-31 23:29:30 +0530

    Challenge 34 PrimeNumber done

---
+++ Challenges/8_Challenge28-39/34 PrimeNumber.java
@@ -0,0 +1,41 @@
+
+// 34. Create a program to check whether a given number is prime.
+
+import java.util.Scanner;
+
+class  PrimeNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Wecome to Prime NUmber Checker!!");
+
+        System.out.print("please enter your number : ");
+        int first = input.nextInt();
+        
+        boolean isPrime = isPrime(first);
+        if (isPrime){
+            System.out.println("Your Number is Prime");
+        }
+        else{
+            System.out.println("Your Number is Not Prime");
+        }
+    }
+
+    public static boolean isPrime(int num){
+
+        int i = 2;
+        while (i < num) {
+            if(num % i == 0){
+                return false;
+            }
+            i++;
+            
+        }
+        return true;
+    }
+    
+}
+
+// Output :
+// Wecome to Prime NUmber Checker!!
+// please enter your number : 67
+// Your Number is Prime
