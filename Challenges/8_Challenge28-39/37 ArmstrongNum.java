170dc10dac22ed10a7f2445ac7b4401bdbadcdd6
satya <satyanandgupta845@gmail.com>
2024-06-05 17:25:41 +0530

    challenge pending

--- Challenges/8_Challenge28-39/37 ArmstrongNum.java
+++ Challenges/8_Challenge28-39/37 ArmstrongNum.java
@@ -1,9 +1,15 @@
 
 // 37. Create a program to check if a number is an Armstrong number.
 
+import java.util.Scanner;
 
- class  ArmstrongNum {
+class  ArmstrongNum {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Check Armstrong Number!!");
+
+        System.out.print("Please Enter Your Number : ");
+        int num = input.nextInt();
         
     }
     
