716f652dcebce172670513e1d16448c85c645854
satya <satyanandgupta845@gmail.com>
2024-06-06 21:47:27 +0530

    File created for Challenge 38 Palindrome

---
+++ Challenges/8_Challenge28-39/38 Palindrome.java
@@ -0,0 +1,10 @@
+// 38. Create a program to verify if a number is a palindrome.
+
+import java.util.Scanner;
+
+class  Palindrome {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+    
+}
