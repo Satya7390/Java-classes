d6bea9359cf9e6fd3d03cc94eb55fcf0297e079c
satya <satyanandgupta845@gmail.com>
2024-06-07 23:11:13 +0530

    Challenge 37 done Armstrong Number

--- Challenges/8_Challenge28-39/37 ArmstrongNum.java
+++ Challenges/8_Challenge28-39/37 ArmstrongNum.java
@@ -10,7 +10,60 @@ class  ArmstrongNum {
 
         System.out.print("Please Enter Your Number : ");
         int num = input.nextInt();
+        boolean isArmstrong = isArmstrong(num);
+
+        if (isArmstrong) {
+            System.out.println("Your number is Armstrong");
+        }
+        else{
+            System.out.println("Your number is not Armstrong");
+        }
         
+    }
+
+    public static boolean isArmstrong(int num){
+        int Digits = noOfDigits(num) ;
+        int copyNumber = num;
+        int finalNumber = 0 ;
+        while (num > 0) {
+            int lastDigit = num % 10;
+            finalNumber += pow(lastDigit, Digits);
+            num /= 10 ;
+        }
+        // System.out.println(num); // for debugging purpose
+        return finalNumber == copyNumber;
+    }
+
+    // for calculating power
+
+    public static int pow(int num1 , int num2){
+        int result = 1;
+        int i = 0;
+        while (i < num2) {
+            result *= num1;
+            i++;
+        }
+        return result;
+    }
+
+    // for number of digit count
+    
+    public static int noOfDigits(int num){
+        int Digits = 0;
+        while (num > 0) {
+            num /= 10;
+            Digits++;
+        }
+        return Digits;
     }
     
 }
+
+// Outputs :
+// Welcome to Check Armstrong Number!!
+// Please Enter Your Number : 153
+// Your number is Armstrong
+
+// Welcome to Check Armstrong Number!!
+// Please Enter Your Number : 15
+// Your number is not Armstrong
