c7cb734280bfe83ee949b35df39eca3e1fde801e
satya <satyanandgupta845@gmail.com>
2024-06-12 08:55:12 +0530

    Concept of Two dimensional array is done

---
+++ Lectures/Array/TwoDimensionalArray.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class TwoDimensionalArray {
+    public static void main(String[] args) {
+        int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+        // Traversal
+        int i = 0;
+        while (i < arr.length) {
+
+            int j = 0;
+            while (j < arr[i].length) {
+                System.out.print(arr[i][j] + " ");
+                j++;
+
+            }
+            System.out.println();
+            i++;
+
+        }
+    }
+
+}
