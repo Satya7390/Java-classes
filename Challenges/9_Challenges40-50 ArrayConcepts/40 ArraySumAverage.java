8ff9da470b0bbd1d133e3be6523796c58a54fef5
satya <satyanandgupta845@gmail.com>
2024-06-13 22:16:52 +0530

    Created file for next challenges

---
+++ Challenges/9_Challenges40-50 ArrayConcepts/40 ArraySumAverage.java
@@ -0,0 +1,11 @@
+
+// 40. Create a program to find the sum and average of all elements in an array.
+
+import java.util.Scanner;
+
+class ArraySumAverage {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+    
+}
