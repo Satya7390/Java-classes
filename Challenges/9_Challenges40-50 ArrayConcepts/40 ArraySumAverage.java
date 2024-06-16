3c4f4c86c44b86fef8fd351cc677cfef70ec32ea
satya <satyanandgupta845@gmail.com>
2024-06-16 23:58:43 +0530

    Challenge 40 Done Sum and Average of the Array

--- Challenges/9_Challenges40-50 ArrayConcepts/40 ArraySumAverage.java
+++ Challenges/9_Challenges40-50 ArrayConcepts/40 ArraySumAverage.java
@@ -8,6 +8,34 @@ class ArraySumAverage {
       
         System.out.println("Welcome to Array Sum and Average!!");
         int [] numArray = ArrayUtility.inputArray();
+        long sum = sum(numArray);
+        double avg = average(numArray);
+
+        System.out.println("Sum of the number is : "+sum);
+        System.out.println("Average of the number is : "+avg);
     }
+
+
+    // for find Array Sum
+
+    public static long sum( int []  numArray){
+        int i = 0;
+        int sum = 0;
+        while (i < numArray.length) {
+            sum += numArray[i];
+            i++;
+            
+        }
+        return sum;
+    }
+
+// for find Array Average
+   
+public static double average(int [] numArray){
+
+    double sum = sum(numArray);
+
+    return sum/numArray.length;
+}
     
 }
