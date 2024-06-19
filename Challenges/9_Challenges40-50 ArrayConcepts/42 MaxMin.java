0aaa38bc11f5b78fcb0cfde5e258cbafa7b1a4be
satya <satyanandgupta845@gmail.com>
2024-06-19 20:39:11 +0530

    file created for next challenge

--- Challenges/9_Challenges40-50 ArrayConcepts/42 MaxMin.java
+++ Challenges/9_Challenges40-50 ArrayConcepts/42 MaxMin.java
@@ -2,10 +2,11 @@
 
 // 42. Create a program to find the maximum and minimum element in an array.
 
+import java.util.Scanner;
 
- class  MaxMin {
+class  MaxMin {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
     }
     
 }
