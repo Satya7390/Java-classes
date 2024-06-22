02c5aea4fbaf580d089ad4209553c46b2f87ee8f
satya <satyanandgupta845@gmail.com>
2024-06-22 17:30:43 +0530

    Challenge Pending

--- Challenges/9_Challenges40-50 ArrayConcepts/43 SortArray.java
+++ Challenges/9_Challenges40-50 ArrayConcepts/43 SortArray.java
@@ -1,3 +1,28 @@
- class  SortArray {
-    
+
+// 43. Create a program to check if the given array is sorted.
+
+import java.util.Scanner;
+
+class SortArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Array Sorting Checker!!");
+        int[] numArray = ArrayUtility.inputArray();
+
+        // SortArray(numArray);
+    }
+
+    public static boolean AscendingOrder(int[] numArray){
+        int min = numArray[1];
+        int max = numArray[1];
+        int i = 1;
+        while (i < numArray.length) {
+            if (min < numArray[i] || max > numArray[i]) {
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+
 }
