47251e48f9e1f5721fa2ce32550e758f1e331a0f
satya <satyanandgupta845@gmail.com>
2024-06-29 21:46:33 +0530

    pending

--- Challenges/9_Challenges40-50 ArrayConcepts/44 NewArray.java
+++ Challenges/9_Challenges40-50 ArrayConcepts/44 NewArray.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 class  NewArray {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println();
     }
     
 }
