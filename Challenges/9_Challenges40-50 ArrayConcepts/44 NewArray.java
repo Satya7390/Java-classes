19afdfe6b3cced31511b223ed9a196b27e407fb4
satya <satyanandgupta845@gmail.com>
2024-06-30 22:11:19 +0530

    Challenge pending

--- Challenges/9_Challenges40-50 ArrayConcepts/44 NewArray.java
+++ Challenges/9_Challenges40-50 ArrayConcepts/44 NewArray.java
@@ -6,7 +6,10 @@ import java.util.Scanner;
 class  NewArray {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println();
+        System.out.println("Welcome to Array Deletion!!\n");
+        int [] numArray = ArrayUtility.inputArray();
+        System.out.println("Now , Enter your number which you want to delete : ");
+        int numToDelete = input.nextInt();
     }
     
 }
