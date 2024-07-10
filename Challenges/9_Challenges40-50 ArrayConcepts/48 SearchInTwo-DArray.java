bddfad06a09fd1b38f9e00b2e522c7334a6904df
satya <satyanandgupta845@gmail.com>
2024-07-10 21:26:48 +0530

    File created

---
+++ Challenges/9_Challenges40-50 ArrayConcepts/48 SearchInTwo-DArray.java
@@ -0,0 +1,10 @@
+
+// 48. Create a program to search an element in a 2-D array.
+
+import java.util.Scanner;
+
+class SearchInTwoDArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+}
