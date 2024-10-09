fa13a1445b28cbc6a95ae2fbd12250c150f06e6b
satya <satyanandgupta845@gmail.com>
2024-10-09 19:34:49 +0530

    Challenge 51 Book class for a library system is done

--- Challenges/10_challenge51to52/51 Book.java
+++ Challenges/10_challenge51to52/51 Book.java
@@ -32,6 +32,51 @@ class Book {
     }
 
     Book (String isbn){ 
-        this( isbn , title:"unknown" , author:"unknown" );
+        this( isbn ,"unknown" , "unknown" );
+    }
+
+    static int getTotalBooks(){
+        return totalNoOfBooks ;
+    }
+    void borrowBook(){
+        if (isBorrowed) {
+            System.out.println("Book is already borrowed");
+        }else{
+            this.isBorrowed = true;
+            System.out.println("Enjoy the " +this.title +" Book!!");
+        }
+    }
+
+    void returnBook(){
+        if (isBorrowed) {
+            this.isBorrowed = false;
+            System.out.println("Hope you enjoyed the book!! , please leave a review");
+        }else{
+            System.out.println("This book is already returned in the library!! Thank you for your Privious time...");
+        }
+    }
+
+    public static void main(String[] args) {
+        Book ThingsOfDesign = new Book("1" , "Design" , "Unknown");
+        Book myBook = new Book("2");
+        System.out.println("Available books in the library is : "+Book.totalNoOfBooks);
+        ThingsOfDesign.borrowBook();
+        myBook.borrowBook();
+        ThingsOfDesign.borrowBook();
+        ThingsOfDesign.returnBook();
+        ThingsOfDesign.borrowBook();
+        ThingsOfDesign.returnBook();
+        ThingsOfDesign.returnBook();
+
     }
 }
+
+// Outputs : 
+// Available books in the library is : 2
+// Enjoy the Design Book!!
+// Enjoy the unknown Book!!
+// Book is already borrowed
+// Hope you enjoyed the book!! , please leave a review
+// Enjoy the Design Book!!
+// Hope you enjoyed the book!! , please leave a review
+// This book is already returned in the library!! Thank you for your Privious time...
