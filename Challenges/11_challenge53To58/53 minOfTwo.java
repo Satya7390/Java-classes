12b793a0c2fc1f59a318b0da05a7a7838921e0b6
satya <satyanandgupta845@gmail.com>
2024-10-14 09:46:00 +0530

    challenge.txt added

---
+++ Challenges/11_challenge53To58/53 minOfTwo.java
@@ -0,0 +1,9 @@
+// package Challenges.11_challenge53To58;
+
+// 53. Create a program to find the minimum of two numbers.
+
+ class  minOfTwo {
+    public static void main(String[] args) {
+        
+    }
+}
