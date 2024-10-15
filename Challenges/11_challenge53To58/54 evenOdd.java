c0215795398ce3620fa7785475eed550a3b42de4
satya <satyanandgupta845@gmail.com>
2024-10-15 19:31:55 +0530

    Challenge 54 evenOdd done using ternary

--- Challenges/11_challenge53To58/54 evenOdd.java
+++ Challenges/11_challenge53To58/54 evenOdd.java
@@ -1,8 +1,26 @@
 
 // 54. Create a program to find if the given number is even or odd.
 
- class  evenOdd {
+import java.util.Scanner;
+
+class evenOdd {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nWelcome to finding even or odd!!");
+        System.out.print("Enter your number which you want to find even or odd : ");
+        int num = input.nextInt();
+
+        String result = num % 2 == 0 ? "Even" : "Odd";
+        System.out.println("Your given number " + num + " is " + result);
     }
+
 }
+
+// Outputs : 
+// Welcome to finding even or odd!!
+// Enter your number which you want to find even or odd : 5
+// Your given number 5 is Odd
+
+// Welcome to finding even or odd!!
+// Enter your number which you want to find even or odd : 6
+// Your given number 6 is Even
