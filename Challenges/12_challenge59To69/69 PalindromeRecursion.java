bf72a780d9f918ada06c07389f9a5c0e6b47574b
satya <satyanandgupta845@gmail.com>
2024-10-26 23:20:51 +0530

    challenge 69 Palindrome using recursion done

--- Challenges/12_challenge59To69/69 PalindromeRecursion.java
+++ Challenges/12_challenge59To69/69 PalindromeRecursion.java
@@ -2,8 +2,49 @@
 // 69. Create a program using recursion to check if a string is a palindrome using
 // recursion.
 
+import java.util.Scanner;
+
 class PalindromeRecursion {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Palindrome Checker !!");
+        System.out.print("Enter the string to be checked: ");
+        String str = input.next();
 
+        str = str.toLowerCase();
+        System.out.println("Your string is " + (isPalindrome(str) ? "Palindrome" : "not palindrome"));
+    }
+
+    public static boolean isPalindrome(String str) {
+
+        if (str.length() <= 1) { // Base case
+            return true;
+        }
+
+        int lastPos = str.length() - 1;
+        if (str.charAt(0) != str.charAt(lastPos)) {
+            return false;
+        }
+        String newStr = str.substring(1, lastPos); // it will chek from 1 to lastPos-1
+        // System.out.println(newStr);
+        return isPalindrome(newStr);
     }
 }
+
+// Outputs :
+
+// Welcome to Palindrome Checker !!
+// Enter the string to be checked: 121
+// Your string is Palindrome
+
+// Welcome to Palindrome Checker !!
+// Enter the string to be checked: Satya
+// Your string is not palindrome
+
+// Welcome to Palindrome Checker !!
+// Enter the string to be checked: Nitin
+// Your string is Palindrome
+
+// Welcome to Palindrome Checker !!
+// Enter the string to be checked: madam
+// Your string is Palindrome
