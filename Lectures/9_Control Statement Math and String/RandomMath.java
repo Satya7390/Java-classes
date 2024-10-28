c745d1914f7db6637922a6630fcfcce64eb0d987
satya <satyanandgupta845@gmail.com>
2024-10-28 23:58:09 +0530

    random math class concepts done

--- Lectures/9_Control Statement Math and String/RandomMath.java
+++ Lectures/9_Control Statement Math and String/RandomMath.java
@@ -7,9 +7,32 @@ public class RandomMath {
         System.out.println(Math.round(55.06));
         System.out.println(Math.round(55.7));
         System.out.println("------------------------------");
+
         for (int i = 0; i < 10; i++) {
-            System.out.println(Math.round(Math.random()*100));
+            int random = (int) Math.round(Math.random() * 100);
+            System.out.println(random);
         }
-        
+
     }
 }
+
+
+// Outputs : 
+
+// 56
+// 46
+// 78.0
+// 77.0
+// 55
+// 56
+// ------------------------------
+// 6
+// 99
+// 93
+// 62
+// 42
+// 26
+// 85
+// 84
+// 91
+// 7
