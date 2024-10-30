e474ad023cc568c5e18371956288f48424d8b0fb
satya <satyanandgupta845@gmail.com>
2024-10-30 20:29:51 +0530

    toString concepts done

--- Lectures/9_Control Statement Math and String/ToString.java
+++ Lectures/9_Control Statement Math and String/ToString.java
@@ -2,13 +2,46 @@ public class ToString {
     int noOfWheels ;
     int noOfDoors;
     int maxSpeed;  
-    int model;
+    String model;
     String carName;
-    String carNumber;
     String company;
 
-    public Car(int noOfWheels, int noOfDoors , int maxSpeed , int model,  String carName, String carNumber,  String company);
 
-    this.noOfWheels = =noOfWheels;
 
+     // this is easly generated through the vs code 
+     //extension source action -> Generate Constructor
+
+    public ToString(int noOfWheels, int noOfDoors, int maxSpeed, String model, String carName, String company) { 
+
+        this.noOfWheels = noOfWheels;
+        this.noOfDoors = noOfDoors;
+        this.maxSpeed = maxSpeed;
+        this.model = model;
+        this.carName = carName;
+        this.company = company;
+    }
+
+
+    
+     // this is easly generated through the vs code 
+     //extension source action -> Generate toString()
+
+    @Override
+    public String toString() {
+        return "My car name is : "+carName;
+    }
+    
+
+    public static void main(String[] args) {
+        ToString Swift = new ToString(4,2,120,"UP5735","Swift","Maruti");
+        System.out.println(Swift);
+              
+    }
+
+  
   }
+
+
+//   Outputs : 
+
+//   My car name is : Swift
