1537c7c33b17ed6ef47a921bdf542de68a12afa5
satya <satyanandgupta845@gmail.com>
2024-10-31 22:21:50 +0530

    Happy diwali to my journey

---
+++ Lectures/diwali.java
@@ -0,0 +1,14 @@
+public class diwali {
+    public static void main(String[] args) {
+        String today = "Diwali"; // Define the 'today' variable
+        
+        if (today.equals("Diwali")) {
+            System.out.println("Happy Diwali to you and your Family! 🎉🪔");
+        }
+    }
+}
+
+
+// Outputs: 
+
+// Happy Diwali to you and your Family! 🎉🪔
