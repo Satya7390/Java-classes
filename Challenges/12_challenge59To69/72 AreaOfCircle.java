60044c8da5be96c9856635c19b229fda27ea8186
satya <satyanandgupta845@gmail.com>
2024-11-04 21:39:00 +0530

    file created for next challenge

---
+++ Challenges/12_challenge59To69/72 AreaOfCircle.java
@@ -0,0 +1,9 @@
+
+// 72. Calculate the area and circumference of a circle for a given radius
+// using Math.PI
+
+class  AreaOfCircle {
+    public static void main(String[] args) {
+        
+    }
+}
