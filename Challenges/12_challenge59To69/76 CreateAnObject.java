b4fef564a853c0187570d506de4b58b1b5340496
satya <satyanandgupta845@gmail.com>
2024-11-11 16:57:56 +0530

    challenge 76  done

--- Challenges/12_challenge59To69/76 CreateAnObject.java
+++ Challenges/12_challenge59To69/76 CreateAnObject.java
@@ -1,9 +1,18 @@
 
 // 76. Create an object with final fields and a constructor to initialize
 // them.
+class CreateAnObject {
 
-class  CreateAnObject {
-    public static void main(String[] args) {
-        
+    final int noOfWheels;
+    final int noOfDoors;
+    final String model;
+    final String engineInLiters;
+
+    public CreateAnObject(String engineInLiters, String model, int noOfDoors, int noOfWheels) {
+        this.engineInLiters = engineInLiters;
+        this.model = model;
+        this.noOfDoors = noOfDoors;
+        this.noOfWheels = noOfWheels;
     }
+
 }
