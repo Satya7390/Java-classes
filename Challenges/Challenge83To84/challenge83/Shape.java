77c3d863a8509cc70faa3158d0a2ed442e31cdc1
satya <satyanandgupta845@gmail.com>
2024-12-20 00:04:06 +0530

    challenge 83 done

---
+++ Challenges/Challenge83To84/challenge83/Shape.java
@@ -0,0 +1,7 @@
+package challenge83;
+
+public abstract class Shape {
+
+    public abstract double calculateArea();
+
+}
---
+++ Challenges/Challenge83To84/challenge83/Square.java
@@ -0,0 +1,20 @@
+package challenge83;
+
+public class Square extends Shape {
+
+    private final double sideInCms;
+
+    public Square(double sideInCms) {
+        this.sideInCms = sideInCms;
+    }
+
+    public double getSideInCms() {
+        return sideInCms;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.pow(sideInCms, sideInCms);
+    }
+
+}
