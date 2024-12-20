28e99b7c0bfbc4681d4a7f8b314074737eb67fdd
satya <satyanandgupta845@gmail.com>
2024-12-20 11:43:43 +0530

    Right Half Pyramid done

---
+++ New Challenges/RightHalfPyramid.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class RightHalfPyramid {
+
+    public static void main(String[] args) {
+        System.out.println("\nWelcome to Right Half Pyramid!!\n");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int num = input.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Outputs: 
+// Welcome to Right Half Pyramid!!
+// Enter Number : 7
+//  *
+//  * *
+//  * * *
+//  * * * *
+//  * * * * *
+//  * * * * * *
+//  * * * * * * *
