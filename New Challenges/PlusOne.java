428608c7cfe16bce8d52f8d2122a672abe974715
satya <satyanandgupta845@gmail.com>
2025-01-09 15:52:23 +0530

    PlusOne challenge done

---
+++ New Challenges/PlusOne.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class PlusOne {
+    public static void main(String[] args) {
+        int digits[] = { 9,9 ,8};
+        System.out.println(Arrays.toString(plusOne(digits)));
+    }
+
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits; 
+            } else {
+                digits[i] = 0; 
+            }
+        }
+        
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1; 
+        return newDigits;
+    }
+}
