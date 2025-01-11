ef81989bccfd1737c371ad01d865e33bbec29f1d
satya <satyanandgupta845@gmail.com>
2025-01-11 23:23:30 +0530

    challenge done

--- New Challenges/Arrays/BubbleSort.java
+++ New Challenges/Arrays/BubbleSort.java
@@ -2,26 +2,28 @@ package Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int arr[] = {5,4,1,3,2};
+        int arr[] = { 5, 4, 1, 3, 2 };
+
         bubbleSort(arr);
         printArr(arr);
     }
 
-    public static void printArr(int arr[]){
+    public static void bubbleSort(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]+" ");
-        }
-        System.out.println();
-    }
-    public static void bubbleSort(int arr []){
-        for(int i = 0; i<arr.length; i++){
-            for(int j = 0; j<arr.length-1-i; j++){
-                if(arr[j] < arr[j+1]){
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swapping
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1]= temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
 }
