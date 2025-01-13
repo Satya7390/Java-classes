99fcb34727f87678a74673e017a648de51196fd4
satya <satyanandgupta845@gmail.com>
2025-01-13 23:58:39 +0530

    selection sort done

--- New Challenges/Arrays/SelectionSort.java
+++ New Challenges/Arrays/SelectionSort.java
@@ -3,5 +3,29 @@ package Arrays;
 public class SelectionSort {
     public static void main(String[] args) {
         int arr[] = { 5, 4, 1, 3, 2 };
+        selectSort(arr);
+        selectPrint(arr);
+
+    }
+
+    public static void selectSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minPos] > arr[j]) {
+                    minPos = j;
+                }
+            }
+            // Swapping
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void selectPrint(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
