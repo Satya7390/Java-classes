7cec47e415af0dfc30f41842cb314c855493719c
satya <satyanandgupta845@gmail.com>
2025-01-20 21:58:54 +0530

    challenge done

--- New Challenges/2DArrays/staircaseSearch.java
+++ New Challenges/2DArrays/staircaseSearch.java
@@ -1,5 +1,24 @@
 public class staircaseSearch {
     public static void main(String[] args) {
-        
+        int matrix[][] = { { 10, 20, 30, 40 }, { 15, 25, 35, 45 }, { 27, 29, 37, 48 }, { 32, 33, 39, 50 } };
+        int key = 10;
+        stairSearchCase(matrix, key);
+    }
+
+    public static boolean stairSearchCase(int matrix[][], int key) {
+        int row = 0, col = matrix[0].length - 1;
+
+        while (row < matrix.length && col >= 0) {
+            if (key == matrix[row][col]) {
+                System.out.println("Key found at " + "(" + row + "," + col + ")");
+                return true;
+            } else if (key < matrix[row][col]) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        System.out.println("Key not found!");
+        return false;
     }
 }
