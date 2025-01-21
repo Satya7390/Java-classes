f8325c169669a2664a08e0e21f5b3003c60af29c
satya <satyanandgupta845@gmail.com>
2025-01-21 22:17:54 +0530

    String concept done

--- New Challenges/2DArrays/staircaseSearch.java
+++ New Challenges/2DArrays/staircaseSearch.java
@@ -1,7 +1,7 @@
 public class staircaseSearch {
     public static void main(String[] args) {
         int matrix[][] = { { 10, 20, 30, 40 }, { 15, 25, 35, 45 }, { 27, 29, 37, 48 }, { 32, 33, 39, 50 } };
-        int key = 10;
+        int key = 33;
         stairSearchCase(matrix, key);
     }
 
---
+++ New Challenges/Strings/InputOutput.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+public class InputOutput {
+    public static void main(String[] args) {
+        // char arr[] = {'a','b'};
+        // String str = "Satya";
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("........");
+        String name = sc.next();
+        String name2 = sc.nextLine();
+        System.out.println(name);
+        System.out.println(name2);
+    }
+
+}
