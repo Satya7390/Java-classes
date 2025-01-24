966041b9cc1c57b7ab76f9b063351d6535a51e9d
satya <satyanandgupta845@gmail.com>
2025-01-24 16:34:16 +0530

    pending substring concept

---
+++ New Challenges/Strings/Substring.java
@@ -0,0 +1,15 @@
+public class Substring {
+    public static void main(String[] args) {
+        String s1 = "Hello Satya!";
+        System.out.println(s1.substring(0, 5));
+        String fruits[] = {"appale","mango","banana"};
+
+        String largest = fruits[0];
+        // for(int i =0 i<fruits.length; i++){
+            
+        //     if(largest.compareToIgnoreCase(fruits[i])<0){
+        //         largest = fruits[i];
+        //     }
+        // }
+    }
+}
