53bae78dbacd07f37cc11c2b10ffe36b73bdd0db
satya <satyanandgupta845@gmail.com>
2025-01-25 22:38:44 +0530

    string done

--- New Challenges/Strings/Substring.java
+++ New Challenges/Strings/Substring.java
@@ -2,14 +2,16 @@ public class Substring {
     public static void main(String[] args) {
         String s1 = "Hello Satya!";
         System.out.println(s1.substring(0, 5));
-        String fruits[] = {"appale","mango","banana"};
+        String fruits[] = { "appale", "mango", "banana","Mmango" };
 
         String largest = fruits[0];
-        // for(int i =0 i<fruits.length; i++){
-            
-        //     if(largest.compareToIgnoreCase(fruits[i])<0){
-        //         largest = fruits[i];
-        //     }
-        // }
+        for (int i = 1; i < fruits.length; i++) {
+
+            if (largest.compareToIgnoreCase(fruits[i]) < 0) {
+                largest = fruits[i];
+            }
+        }
+
+        System.out.println(largest);
     }
 }
