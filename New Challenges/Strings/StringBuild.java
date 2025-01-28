1ea6d8d07a3a64bd461c4f2216538b60215be865
satya <satyanandgupta845@gmail.com>
2025-01-28 20:50:05 +0530

    StringBuilder concept done

---
+++ New Challenges/Strings/StringBuild.java
@@ -0,0 +1,11 @@
+class StringBuild {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("");
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            sb.append(ch);
+        }
+        System.out.println(sb);
+        System.out.println(sb.length());// 26
+
+    }
+}
