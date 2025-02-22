4fd6744bdd2bd208c68e97bda64867608ecb107f
satya <satyanandgupta845@gmail.com>
2025-02-22 21:49:34 +0530

    toUpperCase problem done

--- New Challenges/Strings/ToUpperCase.java
+++ New Challenges/Strings/ToUpperCase.java
@@ -1,9 +1,24 @@
 public class ToUpperCase {
     public static void main(String[] args) {
-        String str = "hello i am satya!!";
+        String str = "hey, i am satya!!";
+        System.out.println(toUpperCase(str));
     }
 
-    public static String UpperCase(String str) {
-        StringBuild sb = new StringBuilder("");
-    }
+    public static String toUpperCase(String str) {
+        StringBuilder sb = new StringBuilder("");
+
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == ' ' && i < str.length() - 1) {
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            } else {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    };
 }
